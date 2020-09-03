97b99410370686a39e6992dc37d72f041f3ce6b4
bufeetu <bufeetu@gmail.com>
2020-09-03 17:38:20 +0800

    go home

--- class_base.py
+++ class_base.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 pd.set_option('display.max_columns', None)
 import numpy as np
+import base64
+import hmac
+import hashlib
+
 class Base():
 
     #基础日期操作
@@ -51,7 +55,8 @@ class Base():
         except Exception as e:
             print("httpGet failed, detail is:%s" % e)
             return {"status": "fail", "msg": "%s" % e}
-    def http_get_request(self, url, params, add_to_headers=None):
+    #火币的基础请求
+    def http_get_request(self,url, params, add_to_headers=None):
         headers = {
             "Content-type": "application/x-www-form-urlencoded",
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
@@ -60,7 +65,7 @@ class Base():
             headers.update(add_to_headers)
         postdata = urllib.parse.urlencode(params)
         try:
-            response = requests.get(url, postdata, headers=headers, timeout=30)
+            response = requests.get(url, postdata, headers=headers, timeout=10)
             if response.status_code == 200:
                 return response.json()
             else:
@@ -68,4 +73,68 @@ class Base():
         except Exception as e:
             print("httpGet failed, detail is:%s" % e)
             return {"status": "fail", "msg": "%s" % e}
+
+    def http_post_request(self,url, params, add_to_headers=None):
+        headers = {
+            "Accept": "application/json",
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
+        }
+        if add_to_headers:
+            headers.update(add_to_headers)
+        postdata = json.dumps(params)
+        try:
+            response = requests.post(url, postdata, headers=headers, timeout=10)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return response.json()
+        except Exception as e:
+            print("httpPost failed, detail is:%s" % e)
+            return {"status": "fail", "msg": "%s" % e}
+
+    def api_key_get(self,url, request_path, params, ACCESS_KEY, SECRET_KEY):
+        method = 'GET'
+        timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+        params.update({'AccessKeyId': ACCESS_KEY,
+                       'SignatureMethod': 'HmacSHA256',
+                       'SignatureVersion': '2',
+                       'Timestamp': timestamp})
+
+        host_name = host_url = url
+        # host_name = urlparse.urlparse(host_url).hostname
+        host_name = urllib.parse.urlparse(host_url).hostname
+        host_name = host_name.lower()
+
+        params['Signature'] = self.createSign(params, method, host_name, request_path, SECRET_KEY)
+        url = host_url + request_path
+        return self.http_get_request(url, params)
+
+    def api_key_post(self,url, request_path, params, ACCESS_KEY, SECRET_KEY):
+        method = 'POST'
+        timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+        params_to_sign = {'AccessKeyId': ACCESS_KEY,
+                          'SignatureMethod': 'HmacSHA256',
+                          'SignatureVersion': '2',
+                          'Timestamp': timestamp}
+
+        host_url = url
+        # host_name = urlparse.urlparse(host_url).hostname
+        host_name = urllib.parse.urlparse(host_url).hostname
+        host_name = host_name.lower()
+        params_to_sign['Signature'] = self.createSign(params_to_sign, method, host_name, request_path, SECRET_KEY)
+        url = host_url + request_path + '?' + urllib.parse.urlencode(params_to_sign)
+        return self.http_post_request(url, params)
+
+    def createSign(self,pParams, method, host_url, request_path, secret_key):
+        sorted_params = sorted(pParams.items(), key=lambda d: d[0], reverse=False)
+        encode_params = urllib.parse.urlencode(sorted_params)
+        payload = [method, host_url, request_path, encode_params]
+        payload = '\n'.join(payload)
+        payload = payload.encode(encoding='UTF8')
+        secret_key = secret_key.encode(encoding='UTF8')
+        digest = hmac.new(secret_key, payload, digestmod=hashlib.sha256).digest()
+        signature = base64.b64encode(digest)
+        signature = signature.decode()
+        return signature
 
--- main.py
+++ main.py
@@ -11,106 +11,21 @@ from class_turtle import Turtle
 from class_policyshunshiwg import Policyshunshiwg
 from calss_kline import Kline
 import pdb
-'''
-param={
-        'zhangshu_base':0.1,
-        'jiange_atr':0.2,
-        'geshu_ni': 2,
-        'zhiying':6,
-        'huiche':30,
-    }
-    
-创建网格
-    方向 由ma
-    张数计算
-    间隔计算
-    顺势20格 逆势根据参数
-开仓交易
-    看多时,突破DC20 开始多头网格
-    一直顺,顺势开,冻结逆势  盈利=顺势开的
-    一直逆,逆势开,冻结顺势,破格止损=逆势开的
-    先顺后逆,顺势开,逆势不开,破格止损=顺势开的
-    先逆后顺,逆势开,顺势不开,顺势开  盈利=逆势+部分顺势
-    
-    注意
-        开一格后,停止对面一格
-        新网格必须破dc20才继续开    
-全局交易
-    牛市一路涨,开多,止盈或者止损,等继续新高
-    牛市一路跌,止损一次后,休眠
-    熊市一路跌,开空,止盈或者止损后,等继续新低
-    熊市一路涨,止损一次后,休眠
-全局概率
-    止盈足够大,止损可以大一些.
-风险分析
-    牛市,破dc20up后,立马跌,
-    熊市,破dc20dn后,立马涨,
-    拉的过快,开单不及时
-    
-
-
-
-
-新增,
-超过初始2ATR止损
-
-策略明细:
-顺1逆2
-顺势开,就停止逆势等张数
-逆势开,就停止顺势等张数
-追踪止盈
-逆势破格止损
-
-优势:
-一直顺势,
-一直逆势,  破格止损,亏损=逆势开的
-先顺后逆,逆势没开,顺势被套,破格止损,亏损=顺势开的
-先逆后顺,逆势开的盈利,顺势中断,顺势开仓
-
-先顺后逆后顺,开顺 不开逆  继续开顺=一直顺
-线逆后顺后逆,开逆 不开顺,继续开逆=一直逆
-
-止盈止损后要突破极值 继续开
-
-场景:
-牛市一直涨,一直顺势
-一路开多
-牛顶一直跌,逆势止损
-一路休息
-熊市一直跌,一直顺势
-一路开空
-熊市一直涨,逆势止损
-一路休息
-一直涨,打止损后,继续涨
-
-参数要求
-
-
-
-'''
-
-
+from HuobiDMService import HuobiDM
 
 def main():
-    robot=Kline()
-    robot.kline_daytomin('btc')
-    robot.kline_daytomin('eth')
-    robot.kline_daytomin('eos')
-    robot.kline_daytomin('ltc')
-    robot.kline_daytomin('etc')
-    # os.system('shutdown -s -f -t 59')
+    robot=HuobiDM()
+    robot.order_piliang(
+        coinname='eos',
+        price_start=3.123,
+        jiange=0.002,
+        times=200,
+        volume=1,
+        direction='sell',
+        offset='open',
+        jingdu=3
+    )
 
-
-
-    # robot=Policyshunshiwg()
-    # param={
-    #     'zhangshu_base':0.05,
-    #     'jiange_atr':0.2,
-    #     'zhisun_atr': 2,
-    #     'zhiying':9,
-    #     'huiche':30,
-    # }
-    # robot.start('eth',"2018-06-18 23:40","2020-06-18 23:59",param)
 if __name__ == '__main__':
     main()
     exit()
