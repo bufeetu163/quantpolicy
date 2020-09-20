fb0592983a76b4824ed4c7610116d3ab22fab16a
bufeetu <bufeetu@gmail.com>
2020-09-20 20:46:31 +0800

    分支测试

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import pdb
 from HuobiDMService import HuobiDM
 from class_policywgma60ma91 import Policywgma60ma91
 from class_policyccg import Policyccg
+#dev
 def piliang_order():
     dm = HuobiDM('bb744223-32147a86-56597f7f-ez2xc4vb6n', '8bc9bf60-64d68dfc-2cc3f4f7-b08de')
     # dm.order_piliang(
